d8daa4460128502aa23a3b42247279f3ceb9a367
Paulo Mariano <paulotiagocm@hotmail.com>
2020-01-17 00:59:42 -0300

    react navigation

--- mobile/App.js
+++ mobile/App.js
@@ -1,31 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Picker } from 'react-native';
+import Routes from './src/Routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>My first app with React Native!</Text>
-      <Picker
-        selectedValue={null}
-        style={{height: 50, width: 100}}
-        onValueChange={(itemValue, itemIndex) =>
-          null
-        }>
-        <Picker.Item label="Java" value="java" />
-        <Picker.Item label="PHP" value="js" />
-      </Picker>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fa8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 16
-  }
-});
