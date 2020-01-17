8988ecdc025dc97dfe61c745a53cc38dc7506aeb
Paulo Mariano <paulotiagocm@hotmail.com>
2020-01-16 22:30:46 -0300

    create and get routes

--- backend/README.md
+++ backend/README.md
@@ -12,7 +12,7 @@ docker run \
     -e MONGO_INITDB_ROOT_PASSWORD=secretpassadmin \
     -d \
     mongo:4
-
+docker start mongodbd
 docker exec -it mongodbd \
     mongo --host 192.168.99.100 -u admin -p secretpassadmin --authenticationDatabase admin \
     --eval "db.getSiblingDB('developers').createUser({user: 'paulotiago', pwd: 'secretpass', roles: [{role: 'readWrite', db: 'developers'}]})"
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,34 @@
 const Router = require('express')
 const dev = require('./models/dev')
+const axios = require('axios')
 
 const routes = Router()
 
-routes.post('/devs', (request,response) => {
-    dev.create({
-        name: 'fixed-post',
-        github_username: 'fixed-post',
-        bio: 'fixed-post',
-        avatar_url: 'fixed-post',
-        techs: 'fixed-post',
-    })
-    return response.json({message:'posting to users'})
+routes.post('/devs', async (request,response) => {
+    const { github_username } = request.body
+    const dataGithub = await axios.get(`https://api.github.com/users/${github_username}`)
+        .catch(err => {
+            console.log('error',err)
+            return response.json({
+                message:'error to save developer',
+                dataSaved:github_username,
+                error: err
+            })
+        })
+    const newDeveloper = {
+        name: dataGithub.data.name,
+        github_username: dataGithub.data.login,
+        bio: dataGithub.data.bio,
+        avatar_url: dataGithub.data.avatar_url,
+        techs: dataGithub.data.public_repos,
+    }
+    dev.create(newDeveloper)
+    return response.json({message:'created developer',dataSaved:newDeveloper})
+})
+
+routes.get('/allDevs', async (request,response) => {
+    const allDevs = await dev.distinct('name')
+    return response.json({message:'search developers',dataGet:allDevs})
 })
 
 module.exports = routes
