a5b336f3f8e868cbcda9ea04e0f3aea0bf0df957
Paulo Mariano <paulotiagocm@hotmail.com>
2020-01-16 23:27:29 -0300

    validate to create

--- backend/src/controllers/devController.js
+++ backend/src/controllers/devController.js
@@ -4,22 +4,25 @@ const axios = require('axios')
 module.exports = {
     async store(request,response) {
         const { github_username, techs, latitude, longitude } = request.body
-        const githubResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-        const {name = login, avatar_url, bio} = githubResponse.data
-        const techArray = techs.split(',').map(tech => tech.trim())
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude]
+        let developer = await dev.findOne({ github_username })
+        if (!developer) {
+            const githubResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+            const {name = login, avatar_url, bio} = githubResponse.data
+            const techArray = techs.split(',').map(tech => tech.trim())
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+            developer = await dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techArray,
+                location
+            })
         }
-        const newDeveloper = await dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techArray,
-            location
-        })
-        return response.json(newDeveloper)
+        return response.json(developer)
     },
     async getAll(request,response) {
         const allDevs = await dev.distinct('name')
