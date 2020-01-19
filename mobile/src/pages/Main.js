eb689354678c199528ef18346b91a7206469f0c1
Paulo Mariano <paulotiagocm@hotmail.com>
2020-01-18 23:01:23 -0300

    callot from avatar name tech and bio

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import MapView, { Marker } from 'react-native-maps'
-import { StyleSheet, Image } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps'
+import { StyleSheet, Image, View, Text } from 'react-native'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
 function Main() {
@@ -36,6 +36,14 @@ function Main() {
             <Marker coordinate={{latitude:-23.4894222,longitude:-47.4638434}}>
                 <Image style={styles.avatar}
                     source={{uri:'https://yt3.ggpht.com/a/AGF-l7_gRI0RdRC_VNg535o0C21ltP0eTFhi4rjRmw=s88-c-k-c0xffffffff-no-rj-mo'}} />
+
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Name fixed</Text>
+                        <Text style={styles.devTechs}>Techs</Text>
+                        <Text style={styles.devBio}>Bio</Text>
+                    </View>
+                </Callout>
             </Marker>
         </MapView>
     )
@@ -52,6 +60,20 @@ const styles = StyleSheet.create({
         borderWidth: 4,
         borderRadius: 4,
         borderColor: '#a311ea'
+    },
+    callout: {
+        width: 260
+    },
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    devTechs: {
+        color: '#333',
+        marginTop: 5
+    },
+    devBio: {
+        marginTop: 5
     }
 })
 
